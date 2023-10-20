aa48da8b54fd9ebd29f8a26614c2e208b4444230
Guilherme Sampaio <guisamfurquim@gmail.com>
2023-10-19 22:18:03 -0300

    Mudança de classes
    Mudança da classe de "Chegada" para "Status" e exclusão da classe "Partida"

--- src/Status.java
+++ src/Status.java
@@ -1,16 +1,24 @@
-public class Partida extends Aviao{
+public class Status extends Aviao{
 
     protected String destino;
+    protected String origem;
     protected String n_Voo;
     protected String empresa;
     protected int portao;
-
-    protected String horario_Partida;
+    protected String horario_Voo;
     protected int qnt_Passageiros;
 
+    
 
-    public Partida(String fabricante, String modelo, String porte, String empresa, int limite_Passageiros, String identificador) {
+    public Status(String fabricante, String modelo, String porte, String empresa, int limite_Passageiros, String identificador, String destino, String origem, String n_Voo, String empresa1, int portao, String horario_Voo, int qnt_Passageiros) {
         super(fabricante, modelo, porte, empresa, limite_Passageiros, identificador);
+        this.destino = destino;
+        this.origem = origem;
+        this.n_Voo = n_Voo;
+        this.empresa = empresa1;
+        this.portao = portao;
+        this.horario_Voo = horario_Voo;
+        this.qnt_Passageiros = qnt_Passageiros;
     }
 
     public String getN_Voo() { return n_Voo; }
@@ -20,7 +28,7 @@ public class Partida extends Aviao{
     public String getEmpresa() { return empresa; }
 
     public int getPortao() { return portao; }
-    public String getHorario_Partida(){ return horario_Partida; }
+    public String getHorario_Partida(){ return horario_Voo; }
 
     public int getPassageiros() { return qnt_Passageiros;}
 
